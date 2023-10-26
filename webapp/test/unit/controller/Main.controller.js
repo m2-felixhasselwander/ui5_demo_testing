9cbb215747fe47f7fe3735ca5c593fefa34de732
Felix Hasselwander <Felix.Hasselwander@mindsquare.de>
2023-10-26 10:28:41 +0000

    Init Test view stub

--- webapp/test/unit/controller/Main.controller.js
+++ webapp/test/unit/controller/Main.controller.js
@@ -1,16 +1,37 @@
 /*global QUnit*/
 
 sap.ui.define([
-	"demo_project/controller/Main.controller"
-], function (Controller) {
+	"demo_project/controller/Main.controller",
+	"sap/ui/base/ManagedObject",
+	"sap/ui/core/mvc/Controller",
+	"sap/ui/model/json/JSONModel"
+], function (MainController, ManagedObject, Controller, JSONModel) {
 	"use strict";
 
-	QUnit.module("Main Controller");
+	QUnit.module("Main Controller", {
+
+		beforeEach: function () {
+			this.oAppController = new MainController();
+			this.oViewStub = new ManagedObject({});
+			sinon.stub(Controller.prototype, "getView").returns(this.oViewStub);
+
+			this.oJSONModelStub = new JSONModel({
+				todos: []
+			});
+			this.oViewStub.setModel(this.oJSONModelStub);
+		},
+
+		afterEach: function () {
+			Controller.prototype.getView.restore();
+
+			this.oViewStub.destroy();
+		}
+	});
 
 	QUnit.test("I should test the Main controller", function (assert) {
-		var oAppController = new Controller();
-		oAppController.onInit();
-		assert.ok(oAppController);
+
+		this.oAppController.onInit();
+		assert.ok(this.oAppController);
 	});
 
 });
